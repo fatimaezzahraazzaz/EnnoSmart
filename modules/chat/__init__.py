c90395b06df08dfb2d2f7c74c091e4f1950d9c38
fatimaezzahraazzaz <fatimaezzahramdl8@gamil.com>
2026-06-19 11:04:15 +0100

    remove unused old modules from git

--- modules/chat/__init__.py
+++
@@ -1,6 +0,0 @@
-# modules/chat/__init__.py
-
-from modules.chat.enno_chat import EnnoChat, EnnoChatConfig
-from modules.chat.schemas import ChatDecision
-
-__all__ = ["EnnoChat", "EnnoChatConfig", "ChatDecision"]
